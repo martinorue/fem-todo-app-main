ffb25ea64f924d75f99b1a5da16e363ce7a6dd5f
martinorue <martin.orue008@gmail.com>
2023-06-23 16:38:59 -0300

    drag and drop styles

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -7,7 +7,7 @@ export function Task ({ task, onCompleteTask, onDeleteTask }) {
     <li className='task'>
         <label className='task-label' htmlFor={taskId}>
             <input type='checkbox' id={taskId} onChange={onCompleteTask} checked={task.completed} />
-            <span className={`${task.completed ? 'task--completed' : ''}`}>{task.name}</span>
+            <span className={`${task.completed ? 'task--completed' : ''} task-text`}>{task.name}</span>
         </label>
         <button className='button-delete-task' onClick={() => onDeleteTask(task.id)}>
           <img src={iconCross} />
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,49 @@
+import { useContext } from 'react'
 import { Task } from './Task'
 import { TaskSummary } from './TaskSummary'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import { ThemeContext } from '../context/ThemeContext'
+
 export function TaskList ({ tasks, onCompleteTask, onDeleteTask, itemsLeft, onClearCompleted, onDragEnd }) {
+  const { isDark } = useContext(ThemeContext)
+  const getItemStyle = (isDragging, draggableStyle) => {
+    const background = isDark ? 'hsl(235, 24%, 19%)' : 'hsl(0, 0%, 98%)'
+    const draggingBg = isDark ? 'hsl(235, 19%, 35%)' : 'hsl(236, 33%, 92%)'
+    const styles = {
+      // some basic styles to make the items look a bit nicer
+      userSelect: 'none',
+
+      // change background colour if dragging
+      background: isDragging ? draggingBg : background,
+
+      // styles we need to apply on draggables
+      ...draggableStyle
+    }
+    return styles
+  }
+
+  const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'hsl(235, 21%, 11%)' : 'hsl(235, 21%, 11%'
+  })
   return (
   <form className='task-list-form' onSubmit={(event) => event.preventDefault()}>
       <ul>
         <DragDropContext onDragEnd={onDragEnd}>
           <Droppable droppableId='tasks'>
-            {(provided) => (
-              <section {...provided.droppableProps} ref={provided.innerRef}>
+            {(provided, snapshot) => (
+              <section {...provided.droppableProps} ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}>
                 {tasks?.map((task, index) =>
                 <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
-                  {(provided) => (
+                  {(provided, snapshot) => (
                     <article
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
-                    ref={provided.innerRef}>
+                    ref={provided.innerRef}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style
+                    )}>
                       <Task
                         task={task}
                         onCompleteTask={() => onCompleteTask(task.id)}

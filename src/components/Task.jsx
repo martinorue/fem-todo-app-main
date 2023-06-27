a7bf5c0c2471064b9562e2046be4a538bc93cf59
martinorue <martin.orue008@gmail.com>
2023-06-27 00:10:35 -0300

    styles

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -10,20 +10,31 @@ export function Task ({ task, onCompleteTask, onDeleteTask }) {
   const cancel = () => {
     console.log('cancel')
   }
+
+  const displayComponent = (
+    <span className={`${task.completed ? 'task--completed' : ''} task-text`}
+    >
+      {task.name}
+    </span>
+  )
+
   return (
 
     <li className='task'>
         <label className='task-label' htmlFor={taskId}>
+          <div className="checkbox-border-wrap">
             <input type='checkbox' id={taskId} onChange={onCompleteTask} checked={task.completed} />
+          </div>
         </label>
             {/* <span className={`${task.completed ? 'task--completed' : ''} task-text`}>{task.name}</span> */}
               <EasyEdit
-                type={Types.TEXT}
+                type={Types.TEXTAREA}
                 saveOnBlur
                 value={task.name}
                 onSave={save}
                 onCancel={cancel}
                 attributes={{ name: 'awesome-input', id: 1 }}
+                displayComponent={displayComponent}
               >
             </EasyEdit>
         <button className='button-delete-task' onClick={() => onDeleteTask(task.id)}>

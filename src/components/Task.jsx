011d8bf618dc426be492431821f95e68939fec41
martinorue <martin.orue008@gmail.com>
2023-06-27 17:24:39 -0300

    add border gradient on dark mode

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,10 @@
-import { useId } from 'react'
+import { useId, useState } from 'react'
 import iconCross from '../assets/images/icon-cross.svg'
 import EasyEdit, { Types } from 'react-easy-edit'
 
 export function Task ({ task, onCompleteTask, onDeleteTask }) {
+  const [showBorder, setShowBorder] = useState(false)
+
   const taskId = useId()
   const save = () => {
     console.log('save')
@@ -18,13 +20,30 @@ export function Task ({ task, onCompleteTask, onDeleteTask }) {
     </span>
   )
 
+  const hoverEnter = () => {
+    setShowBorder(true)
+  }
+  const hoverLeave = () => {
+    setShowBorder(false)
+  }
+
   return (
 
     <li className='task'>
         <label className='task-label' htmlFor={taskId}>
-          <div className="checkbox-border-wrap">
-            <input type='checkbox' id={taskId} onChange={onCompleteTask} checked={task.completed} />
-          </div>
+          {/* <div className="checkbox-border-wrap"> */}
+            <input
+                type='checkbox'
+                id={taskId}
+                onChange={onCompleteTask}
+                checked={task.completed}
+                onMouseEnter={hoverEnter}
+                onMouseLeave={hoverLeave}
+              />
+              <div className={`${showBorder ? 'gradient-border-wrapper' : ''}`}>
+                <div className={`${showBorder ? 'gradient-border' : ''}`}></div>
+              </div>
+          {/* </div> */}
         </label>
             {/* <span className={`${task.completed ? 'task--completed' : ''} task-text`}>{task.name}</span> */}
               <EasyEdit

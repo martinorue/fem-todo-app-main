0f104bdef931e49705649b2bccf94584bd2baa20
martinorue <martin.orue008@gmail.com>
2023-07-04 17:03:30 -0300

    rollback

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -9,7 +9,6 @@ export function TaskList ({ tasks, onCompleteTask, onDeleteTask, onDragEnd, onEd
     const background = isDark ? 'hsl(235, 24%, 19%)' : 'hsl(0, 0%, 98%)'
     const draggingBg = isDark ? 'hsl(235, 19%, 35%)' : 'hsl(236, 33%, 92%)'
     const styles = {
-      listStyleType: 'none',
       // some basic styles to make the items look a bit nicer
       userSelect: 'none',
 
@@ -35,45 +34,39 @@ export function TaskList ({ tasks, onCompleteTask, onDeleteTask, onDragEnd, onEd
         <div className='list-container'>
           <form className='task-list-form' onSubmit={(event) => event.preventDefault()}>
             {tasks.length > 0
-              ? <DragDropContext onDragEnd={onDragEnd}>
+              ? <ul>
+                <DragDropContext onDragEnd={onDragEnd}>
                   <Droppable droppableId='tasks'>
                     {(provided, snapshot) => (
-                      <ul {...provided.droppableProps} ref={provided.innerRef}
+                      <section {...provided.droppableProps} ref={provided.innerRef}
                       style={getListStyle(snapshot.isDraggingOver)}>
                         {tasks?.map((task, index) =>
                         <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
                           {(provided, snapshot) => (
-                            <li
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                              ref={provided.innerRef}
-                              style={getItemStyle(
-                                snapshot.isDragging,
-                                provided.draggableProps.style
-                              )}>
-                              <div
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                role='button'
-                                >
-                              </div>
-                              <div>
-                                <Task
-                                  task={task}
-                                  onCompleteTask={() => onCompleteTask(task.id)}
-                                  onDeleteTask={onDeleteTask}
-                                  onEditTask={onEditTask}
-                                  />
-                              </div>
-                            </li>
+                            <article
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                            style={getItemStyle(
+                              snapshot.isDragging,
+                              provided.draggableProps.style
+                            )}>
+                              <Task
+                                task={task}
+                                onCompleteTask={() => onCompleteTask(task.id)}
+                                onDeleteTask={onDeleteTask}
+                                onEditTask={onEditTask}
+                                />
+                            </article>
                           )}
                         </Draggable>
                         )}
                         {provided.placeholder}
-                      </ul>
+                      </section>
                     )}
                   </Droppable>
                 </DragDropContext>
+              </ul>
               : <p className='no-items-message'>No items on the list</p>
             }
           </form>
